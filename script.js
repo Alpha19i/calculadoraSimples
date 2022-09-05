d99d72995eae80ca7666b359e5bb04ec942c9b16
Camilo Italo <alpha19ci@gmail.com>
2022-09-05 17:44:19 -0300

    created buttons functions

--- script.js
+++ script.js
@@ -4,4 +4,11 @@
     let buttons = document.querySelectorAll('.btn')
     let equal = document.querySelector('.btn-green')
     let clear = document.querySelector('.btn-red')
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
 })();
