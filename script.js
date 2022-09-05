e3f663d8cb5df75a9f38d1cea9f05341afb5e684
Camilo Italo <alpha19ci@gmail.com>
2022-09-05 17:49:46 -0300

    created functions of equal and clean

--- script.js
+++ script.js
@@ -3,12 +3,26 @@
     let screen = document.querySelector('.screen')
     let buttons = document.querySelectorAll('.btn')
     let equal = document.querySelector('.btn-green')
-    let clear = document.querySelector('.btn-red')
+    let clean = document.querySelector('.btn-red')
 
     buttons.forEach(function(button){
         button.addEventListener('click', function(e){
             let value = e.target.dataset.num;
             screen.value += value;
         })
+    })
+    
+    equal.addEventListener('click', function (e){
+        if(screen.value === ''){
+            screen.value = ""
+        }else{
+            let answer = eval(screen.value)
+            screen.value = answer
+        }
+
+    } )
+
+    clean.addEventListener("click",function(e){
+        screen.value = ''
     })
 })();
